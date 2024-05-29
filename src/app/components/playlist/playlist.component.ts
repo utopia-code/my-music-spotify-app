dbcb4e31db69dea6f8492a82524152592847fff2
utopia-code <utopiacode.developer@gmail.com>
2024-05-29 20:24:01 +0200

    PEC5: add playlist component (only basic code to test the service call)

--- src/app/components/playlist/playlist.component.ts
+++ src/app/components/playlist/playlist.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Track } from 'src/app/models/track.interface';
+import { PlaylistService } from 'src/app/services/playlist.service';
 
 @Component({
   selector: 'app-playlist',
   templateUrl: './playlist.component.html',
   styleUrls: ['./playlist.component.css']
 })
-export class PlaylistComponent{
+export class PlaylistComponent implements OnInit {
 
+  tracks: Track[] = []
+
+  constructor(private playlistService: PlaylistService) {}
+
+  ngOnInit(): void {
+
+    this.playlistService.getAllTracks().subscribe(
+      (res) => {
+
+        const items = res.items;
+
+        items.forEach((item: any) => {
+          const track = item.track;
+
+          this.tracks.push(
+            {
+              id: track.id,
+              album: track.album.na,
+              artist: track.artists[0].name,
+              images: track.album.images[0].url,
+              image_width: track.album.images[0].width,
+              image_height: track.album.images[0].height
+            }    
+          );
+        })
+        
+      });
+
+  }
 }
