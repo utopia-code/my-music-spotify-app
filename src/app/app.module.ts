f88f1776b1c76314aac18ed8d3beeb70e661a55f
utopia-code <utopiacode.developer@gmail.com>
2024-05-29 20:23:01 +0200

    PEC5: set providers in app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PlaylistComponent } from './components/playlist/playlist.component';
 
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { TrackComponent } from './components/track/track.component';
+import { AuthInterceptorService } from './services/auth-interceptor.service';
+import { AuthService } from './services/auth.service';
 
 @NgModule({
   declarations: [
@@ -15,9 +18,17 @@ import { TrackComponent } from './components/track/track.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    AuthService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptorService,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
