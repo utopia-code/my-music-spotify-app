ffc9cb83ed469f521e08ff5ffdd1fc99caf2bf75
utopia-code <utopiacode.developer@gmail.com>
2024-05-29 18:49:30 +0200

    PEC5: create components list and detail

---
+++ src/app/components/track/track.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-track',
+  templateUrl: './track.component.html',
+  styleUrls: ['./track.component.css']
+})
+export class TrackComponent {
+}
