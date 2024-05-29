d2d60510e56e50ad220ff6234b8ed163a0f3365d
utopia-code <utopiacode.developer@gmail.com>
2024-05-29 19:01:11 +0200

    PEC5: set routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PlaylistComponent } from './components/playlist/playlist.component';
+import { TrackComponent } from './components/track/track.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: PlaylistComponent },
+  { path: 'track/:id', component: TrackComponent },
+  { path: '**', component: PlaylistComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PlaylistComponent } from './components/playlist/playlist.component';
+
+import { TrackComponent } from './components/track/track.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlaylistComponent,
+    TrackComponent
   ],
   imports: [
     BrowserModule,
