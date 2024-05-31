33032b43880dfa5614cbe95612a8bacbe6d48ecf
utopia-code <utopiacode.developer@gmail.com>
2024-05-31 17:27:22 +0200

    PEC5: set pipe Uri to listening preview tracks

---
+++ src/app/pipes/uri.pipe.ts
@@ -0,0 +1,16 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+
+@Pipe({
+  name: 'uri'
+})
+export class UriPipe implements PipeTransform {
+
+  constructor( private domSanitizer: DomSanitizer ) {}
+
+  transform(value: string): any {
+    const url = 'https://open.spotify.com/embed?uri=';
+    return this.domSanitizer.bypassSecurityTrustResourceUrl( url + value);
+  }
+
+}
